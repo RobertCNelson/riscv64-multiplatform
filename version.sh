c1236216e5a4a30e96e9fae5bf115d9538ab3a78
Robert Nelson <robertcnelson@gmail.com>
2021-02-15 11:00:45 -0600

    kernel v5.11 rebase with: device-tree/etc
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -42,10 +42,10 @@ toolchain="gcc_10_riscv64"
 
 #Kernel
 KERNEL_REL=5.11
-KERNEL_TAG=${KERNEL_REL}-rc7
+KERNEL_TAG=${KERNEL_REL}
 kernel_rt=".x-rty"
 #Kernel Build
-BUILD=${build_prefix}4
+BUILD=${build_prefix}4.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
