18dc0358c024fcf4757635f32fe6c7313ac1dba7
Robert Nelson <robertcnelson@gmail.com>
2021-03-22 10:19:58 -0500

    5.12-rc4-riscv64-x3 release
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -42,10 +42,10 @@ toolchain="gcc_10_riscv64"
 
 #Kernel
 KERNEL_REL=5.12
-KERNEL_TAG=${KERNEL_REL}-rc3
+KERNEL_TAG=${KERNEL_REL}-rc4
 kernel_rt=".x-rty"
 #Kernel Build
-BUILD=${build_prefix}2
+BUILD=${build_prefix}3
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
