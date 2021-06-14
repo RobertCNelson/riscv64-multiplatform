cfa99df5b57eee592a4435c6f306b7a30aece713
Stephen L Arnold <nerdboy@gentoo.org>
2021-06-14 10:11:14 -0700

    patch rebase/refresh on rc6 kernel, remove max-freq sdcard property
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- version.sh
+++ version.sh
@@ -38,10 +38,10 @@ toolchain="gcc_11_riscv64"
 
 #Kernel
 KERNEL_REL=5.13
-KERNEL_TAG=${KERNEL_REL}-rc5
+KERNEL_TAG=${KERNEL_REL}-rc6
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}8
+BUILD=${build_prefix}2
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
