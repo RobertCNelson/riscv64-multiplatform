2450f908a4e19d67264d38d1733beed542eab452
Robert Nelson <robertcnelson@gmail.com>
2021-07-07 16:12:38 -0500

    kernel v5.13.1 rebase with: device-tree/etc WIRELESS_REGDB: https://git.kernel.org/pub/scm/linux/kernel/git/sforshee/wireless-regdb.git/commit/?id=9839e1e389b5d252c61b6bfb11bc798c2125a309
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- version.sh
+++ version.sh
@@ -38,10 +38,10 @@ toolchain="gcc_11_riscv64"
 
 #Kernel
 KERNEL_REL=5.13
-KERNEL_TAG=${KERNEL_REL}
+KERNEL_TAG=${KERNEL_REL}.1
 kernel_rt=".X-rtY"
 #Kernel Build
-BUILD=${build_prefix}12
+BUILD=${build_prefix}12.1
 
 #v5.X-rcX + upto SHA
 #prev_KERNEL_SHA=""
